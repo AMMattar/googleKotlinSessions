59c4884c21499125e9e13522a490bb4ec3aac203
crypto <ahmed.m4business@gmail.com>
2021-09-06 16:35:05 -0700

    s1

---
+++ test.kt
@@ -0,0 +1,16 @@
+fun main() {
+    val age = 5 * 365
+    val name = "Rover"
+    println("Happy Birthday, ${name}!")
+    //let's print a cake
+    println("   ,,,,,   ")
+    println("   |||||   ")
+    println(" =========")
+    println("@@@@@@@@@@@")
+    println("{~@~@~@~@~}")
+    println("@@@@@@@@@@@")
+    //let's print an empty line!
+    println("")
+    println("You are already ${age} days old!, ${name}")
+    println("${age} is the very best age to celebrate!")
+}
